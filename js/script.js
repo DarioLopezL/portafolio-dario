f9d01004312650c77a109c30be0a5c141d0b999e
DarioLopezL <dariiolopez17@gmail.com>
2024-03-13 23:40:15 -0600

    new format send message contact

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function showSuccessMessage() {
+    document.getElementById('success-message').style.display = 'block';
+    setTimeout(function() {
+      document.getElementById('success-message').style.display = 'none';
+    }, 8000);
+  }
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    showSuccessMessage();
+    document.getElementById('contact-form').reset();
+  });
+  
