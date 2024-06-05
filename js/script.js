d80baaf666e129a23070e72dd4cb0efec067364e
DarioLopezL <dariiolopez17@gmail.com>
2024-06-05 13:10:59 -0600

    script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-function showSuccessMessage() {
-    document.getElementById('success-message').style.display = 'block';
-    setTimeout(function() {
-      document.getElementById('success-message').style.display = 'none';
-    }, 8000);
-  }
+
   document.getElementById('contact').addEventListener('submit', function(event) {
     event.preventDefault(); 
     showSuccessMessage();
