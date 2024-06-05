316459c9db8508f3ce4452570c12485c8e254809
DarioLopezL <dariiolopez17@gmail.com>
2024-06-05 12:50:31 -0600

    corrección de formulario agregado

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-function showSuccessMessage() {
+document.addEventListener('DOMContentLoaded', function() {
+  function showSuccessMessage() {
     document.getElementById('success-message').style.display = 'block';
     setTimeout(function() {
       document.getElementById('success-message').style.display = 'none';
     }, 8000);
   }
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
+
+  document.getElementById('contact').addEventListener('submit', function(event) {
+    event.preventDefault();
     showSuccessMessage();
-    document.getElementById('contact').reset();
+    document.getElementById('contact-form').reset();
   });
-  
+});
+
