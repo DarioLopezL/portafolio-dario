bd7d804dc6bc9bf96a3908ed127300e9f1ebbdb7
DarioLopezL <dariiolopez17@gmail.com>
2024-06-05 12:53:32 -0600

    add form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  function showSuccessMessage() {
+function showSuccessMessage() {
     document.getElementById('success-message').style.display = 'block';
     setTimeout(function() {
       document.getElementById('success-message').style.display = 'none';
     }, 8000);
   }
-
   document.getElementById('contact').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault(); 
     showSuccessMessage();
     document.getElementById('contact').reset();
   });
-});
-
+  
