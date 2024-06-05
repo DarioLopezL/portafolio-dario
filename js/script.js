93b8d16c082339679088ab24838a931ed240a812
DarioLopezL <dariiolopez17@gmail.com>
2024-06-05 13:11:42 -0600

    nul message

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-
+/* function showSuccessMessage() {
+    document.getElementById('success-message').style.display = 'block';
+    setTimeout(function() {
+      document.getElementById('success-message').style.display = 'none';
+    }, 8000);
+  }
   document.getElementById('contact').addEventListener('submit', function(event) {
     event.preventDefault(); 
     showSuccessMessage();
     document.getElementById('contact').reset();
   });
-  
+   */
